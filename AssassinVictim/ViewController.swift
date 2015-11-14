909d0cbac59a2fae15568b28a96f671ca142c96f
Mihriban Minaz <mihribanminaz@yahoo.com>
2015-11-14 18:55:39 +0100

    initial commit (just project created)

---
+++ AssassinVictim/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AssassinVictim
+//
+//  Created by Mihriban Minaz on 14/11/15.
+//  Copyright © 2015 Mihriban Minaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
